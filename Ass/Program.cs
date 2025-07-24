8a1b9ed71f62ad47c42dc02cc43e60fc096779d2
m7md9923 <mhmdnsrbrkat0@gmail.com>
2025-07-24 23:18:19 +0300

    Q2

--- Ass/Program.cs
+++ Ass/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Ass.Q1;
+using Ass.Q2;
 
 namespace Ass;
 
@@ -19,7 +20,35 @@ class Program
         // Console.WriteLine($"Length of range : {range2.Length()}");
         
         #endregion
-
+        
+        #region Question 2
+        
+        // List<int> nums = new List<int> { 1, 2, 3, 4, 5 };
+        // Console.WriteLine($"Before:");
+        // foreach (var item in nums)
+        //     Console.Write($"{item}, ");
+        // Console.WriteLine();
+        //
+        // ArrayListReverse.reverse<int>(nums);
+        // Console.WriteLine($"After:");
+        // foreach (var item in nums)
+        //     Console.Write($"{item}, ");
+        // Console.WriteLine();
+        //
+        // List<string> names = new List<string> { "hello", "world", "foo", "bar" };
+        // Console.WriteLine($"Before: ");
+        // foreach (var item in names)
+        //     Console.Write($"{item}, ");
+        // Console.WriteLine();
+        //
+        // ArrayListReverse.reverse<string>(names);
+        // Console.WriteLine($"After: ");
+        // foreach (var item in names)
+        //     Console.Write($"{item}, ");
+        // Console.WriteLine();
+        
+        #endregion
+        
         #endregion
     }
 }
