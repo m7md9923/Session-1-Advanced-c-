c78122c713fa020dc9485e8fcc9f49d27e2b2857
m7md9923 <mhmdnsrbrkat0@gmail.com>
2025-07-24 14:08:32 +0300

    Vid 1

---
+++ Demo/Ex1/Helper.cs
@@ -0,0 +1,70 @@
+﻿namespace Demo.Ex1;
+
+internal class Helper
+{
+    #region Swap [Non Generic]
+    
+    #region Swap By Object
+
+    // public static void Swap(ref object a, ref object b)
+    // {
+    //     object temp = a;
+    //     a = b;
+    //     b = temp;
+    // }
+    
+    #endregion
+
+    #region Swap Two Int Numbers
+    
+    // public static void Swap(ref int a, ref int b)
+    // {
+    //     int temp = a;
+    //     a = b;
+    //     b = temp;
+    // }
+    
+    #endregion
+
+    #region Swap Two Decimal Numbers
+
+    // public static void Swap(ref decimal a, ref decimal b)
+    // {
+    //     decimal temp = a;
+    //     a = b;
+    //     b = temp;
+    // }
+
+    #endregion
+    
+    #region Swap Two Points
+    //
+    // public static void Swap(ref Point a, ref Point b)
+    // {
+    //     Point temp = a;
+    //     a = b;
+    //     b = temp;
+    // }
+    
+    #endregion
+    #endregion
+    
+    #region Swap [Generic]
+
+    // T --> Generic type ==> method level,class level
+    // T --> Method level ==> Compiler will detect the type of T based on parameters
+    
+    
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public static void Print<T>(T a)
+    {
+        Console.WriteLine($"a : {a}");
+    }
+    #endregion
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Demo;
+using Demo.Ex1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Demo
+
+        #region Ex1 [Swap] [int - decimal - point] 
+        
+        // object a = 10 , b = 20;
+        // Console.WriteLine($"a : {a}");
+        // Console.WriteLine($"b : {b}");
+        // Helper.Swap(ref a, ref b);
+        // Console.WriteLine($"After Swap : ");
+        // Console.WriteLine($"a : {a}");
+        // Console.WriteLine($"b : {b}");
+        //
+        // decimal x = 10 , y = 20;
+        // Console.WriteLine($"a : {x}");
+        // Console.WriteLine($"b : {y}");
+        // Helper.Swap(ref x, ref y);
+        // Console.WriteLine($"After Swap : ");
+        // Console.WriteLine($"a : {x}");
+        // Console.WriteLine($"b : {y}");
+        //
+        // // struct if u create ctor ,he does not remove parameterless ctor
+        //
+        // Point p1 = new Point(10, 20);
+        // Point p2 = new Point(30, 40);
+        // Console.WriteLine($"p1 : {p1}");
+        // Console.WriteLine($"p2 : {p2}");
+        // Console.WriteLine($"After Swap : ");
+        // Helper.Swap(ref p1, ref p2);
+        // Console.WriteLine($"p1 : {p1}");
+        // Console.WriteLine($"p2 : {p2}");
+        //
+        // int aa = 10 , bb = 20;
+        // Console.WriteLine($"a : {aa}");
+        // Console.WriteLine($"b : {bb}");
+        // Helper.Swap<int>(ref aa, ref bb);
+        // Console.WriteLine($"After Swap : ");
+        // Console.WriteLine($"a : {aa}");
+        // Console.WriteLine($"b : {bb}");
+        //
+        // Point pp1 = new Point(10, 20);
+        // Helper.Print(pp1);
+        
+        #endregion
+        
+        #endregion
+    }
+    
+}
