fa3e3b3372772afc81832510d4b77cdd5eac8abf
m7md9923 <mhmdnsrbrkat0@gmail.com>
2025-07-24 22:57:30 +0300

    Q1

---
+++ Ass/Program.cs
@@ -0,0 +1,25 @@
+﻿using Ass.Q1;
+
+namespace Ass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Assignment
+
+        #region Question 1
+
+        // Range<int> range = new Range<int>(1, 10);
+        // Console.WriteLine($"Check val if in range : {range.IsInRange(5)}");
+        // Console.WriteLine($"Length of range : {range.Length()}");
+        //
+        // Range<decimal> range2 = new Range<decimal>(2.2M, 10.6M);
+        // Console.WriteLine($"Check val if in range : {range2.IsInRange(5)}");
+        // Console.WriteLine($"Length of range : {range2.Length()}");
+        
+        #endregion
+
+        #endregion
+    }
+}
